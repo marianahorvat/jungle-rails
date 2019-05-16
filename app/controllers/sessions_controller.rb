6e91004c7631caabf803421d51eda84d7f428283
Mariana <mariana.horvat.qa@gmail.com>
2019-05-16 23:24:19 +0000

    User Authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      # If user exists and the password entered by the user is correct.
+      # Save the user id in the browser cookie to keep the user logged in when they navigate this website
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # If the user login is incorrect, they are redirected to the login form.
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+     if @user.save
+      session[:user_id] = @user.id
+      redirect_to [:root], notice: 'user created!'
+    else
+      render :new
+    end
+  end
+
+   def new
+    @user = User.new
+  end
+
+   private
+
+   def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+end
