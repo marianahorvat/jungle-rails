6db5d80444ca4df5525689a294b1eaddebedfdeb
Mariana <mariana.horvat.qa@gmail.com>
2019-05-16 21:20:14 +0000

    Feature: Admin Categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,35 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+      @categories = Category.order(id: :desc).all
+  end
+
+  def new
+      @categories = Category.new
+  end
+
+  def create
+      @categories = Category.new(category_params)
+
+      if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+      else
+      render :new
+      end
+
+  end
+  def category_params
+      params.require(:category).permit(
+      :name,
+      :created_at, 
+      :updated_at
+      )
+    end
+end 
+
+# from schema.rb:
+# create_table "categories", force: :cascade do |t|
+#   t.string   "name"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+# end
